922c92bcca24620490ad131e1227ff1708c38dfc
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-08-30 23:09:35 -0400

    export data from array

---
+++ problema2.rb
@@ -0,0 +1,21 @@
+def read_file(data2)
+    original_data = open("data2").readlines
+    lines = original_data.count
+    array = []
+    lines.times do |i|
+        array << original_data[i].to_i
+    end
+    return array
+end
+
+# print read_file(data2)
+
+data = read_file("data2")
+n = data.count
+n.times do |i|
+    data[i] = 20 if data[i] > 20
+end
+
+# print read_file(data2)
+
+File.write('output', data.join("\n"))
